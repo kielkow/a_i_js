a83e20c47ec52c4fca201af9de6eebb288cd798f
kielkow <matheuskiel@fiorifer.com.br>
2019-07-02 20:59:59 -0300

    Add function btn add

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,4 @@
+/*
 alert('Site carregado!')
 
 let altura, peso, imc
@@ -20,10 +21,41 @@ pacientes.forEach(element => {
         element.querySelector('.info-imc').textContent = imc.toFixed(2)
     }
 });
+*/
+
 
 let btnAdd = document.querySelector('#adicionar-paciente')
 btnAdd.addEventListener('click', function (event){
     event.preventDefault()
+
+    let nome = document.querySelector('#nome').value
+    let peso = document.querySelector('#peso').value
+    let altura = document.querySelector('#altura').value
+    let gordura = document.querySelector('#gordura').value
+    let imc = (peso / (Math.pow(altura, 2)))
+
+    let pacienteTr = document.createElement('tr')
+    let nomeTd = document.createElement('td')
+    let pesoTd = document.createElement('td')
+    let alturaTd = document.createElement('td')
+    let gorduraTd = document.createElement('td')
+    let imcTd = document.createElement('td')
+
+    nomeTd.textContent = nome
+    pesoTd.textContent = peso
+    alturaTd.textContent = altura
+    gorduraTd.textContent = gordura
+    imcTd.textContent = imc.toFixed(2)
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+    pacienteTr.appendChild(imcTd)
+
+    let tabela = document.querySelector('#tabela-pacientes')
+    tabela.appendChild(pacienteTr)
+
     document.getElementById('peso').value = ''
     document.getElementById('altura').value = ''
     document.getElementById('gordura').value = ''
