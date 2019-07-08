caafc63069e50f95a0e1173fdd7278e038d7a58e
kielkow <matheuskiel@fiorifer.com.br>
2019-07-07 22:17:21 -0300

    Add filtro search

--- js/filtro.js
+++ js/filtro.js
@@ -1,8 +1,22 @@
 let filtro = document.querySelector('.filtro-pacientes')
 filtro.addEventListener('input', function (){
     let pacientes = document.querySelectorAll('.paciente-valido')
-    pacientes.forEach(element => {
-        let nome = element.querySelector('.info-nome').textContent
-        console.log(nome)
-    });
+
+    if(filtro.value.length > 0){
+        pacientes.forEach(element => {
+            let nome = element.querySelector('.info-nome').textContent
+            if(nome != filtro.value){
+                element.classList.add('invisible')
+            }
+            else{
+                element.classList.remove('invisible')
+            }
+        });
+    }
+    else{
+        pacientes.forEach(element => {
+            element.classList.remove('invisible')
+        });
+    }
+
 })
