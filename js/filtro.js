5e6346262324ee4eefa203a116815f4f6ce24ecc
kielkow <matheuskiel@fiorifer.com.br>
2019-07-07 23:15:33 -0300

    Add filtro with regex and substr

--- js/filtro.js
+++ js/filtro.js
@@ -5,18 +5,35 @@ filtro.addEventListener('input', function (){
     if(filtro.value.length > 0){
         pacientes.forEach(element => {
             let nome = element.querySelector('.info-nome').textContent
-            if(nome != filtro.value){
-                element.classList.add('invisible')
-            }
-            else{
+            let expressao = new RegExp(filtro.value, 'i')
+            if(expressao.test(nome)){
                 element.classList.remove('invisible')
             }
+            else{
+                element.classList.add('invisible')
+            }
+
+            /* 
+            SEM REGEX
+            
+            let nome = element.querySelector('.info-nome').textContent
+            let comparavel = nome.substr(0, filtro.value.length)
+            let comparavelMin = comparavel.toLowerCase()
+            let inputMin = filtro.value.toLowerCase()
+            if(comparavelMin == inputMin){
+                element.classList.remove('invisible')
+            }
+            else{
+                element.classList.add('invisible')
+            }
+            */
         });
     }
-    else{
+    else{    
         pacientes.forEach(element => {
             element.classList.remove('invisible')
         });
+        
     }
 
 })
