da24d398e025a0753bfac49d9675cfce2ef12024
kielkow <matheuskiel@fiorifer.com.br>
2019-07-08 19:41:27 -0300

    Fixed validacaoImc

--- js/validacaoImc.js
+++ js/validacaoImc.js
@@ -1,5 +1,5 @@
 function validacaoImc(nome,altura, peso, gordura){
-    if ((altura >= 3 || peso <= 0) || (altura <= 0 || peso >= 1000) || (nome === '') || (gordura == '')) {
+    if ((altura >= 3 || peso <= 0) || (altura <= 0 || peso >= 1000) || (nome === '') || (gordura >= 100 || gordura <= 0)) {
         return 'Valor preenchido inválido'
     }
     else {
